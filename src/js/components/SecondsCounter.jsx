92f7cee777a0793c2ae5bd5f1c1df74c6ca3b57e
J0nathan-CR <jacr-work@outlook.com>
2025-07-24 03:13:28 +0000

    Final

--- src/js/components/SecondsCounter.jsx
+++ src/js/components/SecondsCounter.jsx
@@ -1,9 +1,9 @@
 const SecondsCounter = ({ digitOne, digitTwo, digitThree }) => {
     return (
         <div className="d-flex justify-content-center gap-3 fs-1 fw-bold">
-            <div className="bg-warning-subtle rounded-pill p-2">{digitThree}</div>
-            <div className="bg-warning-subtle rounded-pill p-2">{digitTwo}</div>
-            <div className="bg-warning-subtle rounded-pill p-2">{digitOne}</div>
+            <div className="bg-warning-subtle rounded-pill p-3">{digitThree}</div>
+            <div className="bg-warning-subtle rounded-pill p-3">{digitTwo}</div>
+            <div className="bg-warning-subtle rounded-pill p-3">{digitOne}</div>
         </div>
     );
 };
