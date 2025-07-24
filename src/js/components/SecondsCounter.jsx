983a0cc4f196fffb2ac537a3395a739a04a89b09
J0nathan-CR <jacr-work@outlook.com>
2025-07-24 03:51:03 +0000

    Final

--- src/js/components/SecondsCounter.jsx
+++ src/js/components/SecondsCounter.jsx
@@ -1,11 +1,17 @@
 const SecondsCounter = ({ digitOne, digitTwo, digitThree }) => {
     return (
-        <div className="d-flex justify-content-center gap-3 fs-1 fw-bold">
-            <div className="bg-warning-subtle rounded-pill p-3">{digitThree}</div>
-            <div className="bg-warning-subtle rounded-pill p-3">{digitTwo}</div>
-            <div className="bg-warning-subtle rounded-pill p-3">{digitOne}</div>
+        <div className="text-center">
+            <h1 className="mb-4 text-warning">
+                <i className="fa-regular fa-clock me-3 text-warning"></i>
+                Contador Simple
+            </h1>
+
+            <div className="d-flex justify-content-center gap-3 fs-1 fw-bold">
+                <div className="bg-warning-subtle rounded-pill p-3">{digitThree}</div>
+                <div className="bg-warning-subtle rounded-pill p-3">{digitTwo}</div>
+                <div className="bg-warning-subtle rounded-pill p-3">{digitOne}</div>
+            </div>
         </div>
     );
 };
-
 export default SecondsCounter
